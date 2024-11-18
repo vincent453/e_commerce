572acc5990a65944edc24288d973f9aa85322b2d
lawrence vincent <93725993+vincent453@users.noreply.github.com>
2024-11-18 21:58:40 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -45,9 +45,10 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+    image_url: `https://e-commerce-backend-sxn6.onrender.com/images/${req.file.filename}`,
   });
 });
+
 //shema for creating products
 
 const Product = mongoose.model("Product", {
