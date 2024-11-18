d82556c383becaa6e425b87be7e90c2b94c69e81
lawrence vincent <93725993+vincent453@users.noreply.github.com>
2024-11-18 21:35:01 +0100

    Update ShopContext.jsx

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -14,12 +14,12 @@ const ShopContextProvider = (props) => {
     const [cartItems,setCartItems] = useState(getDefaultCart());
     
     useEffect(()=>{
-        fetch('http://localhost:4000/allproducts')
+        fetch('https://e-commerce-backend-sxn6.onrender.com')
         .then((response)=>response.json())
         .then((data)=>setAll_Product(data))
 
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/getcart',{
+            fetch('https://e-commerce-backend-sxn6.onrender.com/getcart',{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
@@ -34,7 +34,7 @@ const ShopContextProvider = (props) => {
    const addToCart = (itemId) =>{
     setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
     if(localStorage.getItem('auth-token')){
-        fetch('http://localhost:4000/addtocart',{
+        fetch('https://e-commerce-backend-sxn6.onrender.com/addtocart',{
             method:'POST',
             headers:{
                 Accept: 'application/form-data',
@@ -50,7 +50,7 @@ const ShopContextProvider = (props) => {
    const removeFromCart = (itemId) =>{
     setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     if(localStorage.getItem('auth-token')){
-        fetch('http://localhost:4000/removefromcart',{
+        fetch('https://e-commerce-backend-sxn6.onrender.com/removefromcart',{
             method:'POST',
             headers:{
                 Accept: 'application/form-data',
